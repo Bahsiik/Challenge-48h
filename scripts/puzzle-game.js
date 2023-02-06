94b05d2312604292c6ac3dd3f3a16da5f24b8c2e
Bahsiik <o.mist1303@gmail.com>
2023-02-06 16:58:49 +0100

    add the second game

---
+++ scripts/puzzle-game.js
@@ -0,0 +1,84 @@
+const rows = 4;
+const columns = 4;
+
+let currTile;
+let otherTile;
+
+let turns = 0;
+
+const imgOrder = ["1", "2", "3", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "100"];
+imgOrder.sort(() => Math.random() - 0.5);
+
+window.onload = function () {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = "../ressources/4x4/" + imgOrder.shift() + ".jpg";
+
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
+            document.getElementById("board").append(tile);
+
+        }
+    }
+}
+
+function dragStart() {
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this;
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("100")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-");
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r === r2 && c2 === c - 1;
+    let moveRight = r === r2 && c2 === c + 1;
+
+    let moveUp = c === c2 && r2 === r - 1;
+    let moveDown = c === c2 && r2 === r + 1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        currTile.src = otherTile.src;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turns").innerText = turns;
+    }
+
+
+}
